f2b5f04fbb899522118c1f7b3ca5276f9971f608
QuintinMakwe <quintinmakwe@gmail.com>
2020-03-20 22:27:09 +0100

    Added validation for the decimal to binary converter

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question("Enter a number: ", function(number) {
-  console.log(`you entered number ${number}`);
+rl.question("Enter a number: ", async function(number) {
+  let convertedNumber = parseInt(number, 10).toString(2);
+  if (convertedNumber.toString() === NaN.toString()) {
+    rl.question("Final try, enter a valid number please: ", async function(
+      number2
+    ) {
+      convertedNumber = await parseInt(number2, 10).toString(2);
+      if (convertedNumber.toString() === NaN.toString()) {
+        console.log("Try again when you're ready to follow instructions");
+        rl.close();
+      } else {
+        console.log(convertedNumber);
+        rl.close();
+      }
+    });
+  } else {
+    console.log(convertedNumber);
+    rl.close();
+  }
 });
